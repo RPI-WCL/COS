ed687fcf0f3f6f13a02b06347c405ca6d5572155
Tom Chestna <tomchestna@gmail.com>
2012-10-06 20:50:30 -0400

    Added a build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+cd common
+make clean
+make
+cd ..
+cd cosManager
+make clean
+make
+cd ..
+cd nodeManager
+make clean
+make
+cd ..
+cd util
+make clean
+make
+cd ..
+cd vmManager
+make clean
+make
+cd ..
